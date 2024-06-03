8b279a056cc29a8d0099b4e36edb2ed224d27d78
SimoneCerri <simone.cerri1994@gmail.com>
2024-06-03 15:59:11 +0200

    add axios's call for put characters in page

---
+++ README.md
@@ -0,0 +1,9 @@
+# TODO #
+
+- Splash-page
+- Characters-page
+    - Cards of characters
+- Choos your character
+    - Name
+    - Class
+    - Etc etc
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      characters: [],
+      base_api_url: 'http://127.0.0.1:8000',
+      base_characters_url: '/api/characters',
+    }
+  },
+  methods: {
+    callApi(url) {
+      axios.get(url).then(response => {
+        console.log(response.data.characters);
+        this.characters = response.data.characters;
+      })
+    }
+  },
+  mounted() {
+    this.callApi(this.base_api_url + this.base_characters_url)
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div v-for="character in characters.data">
+    {{ character.name }}
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
