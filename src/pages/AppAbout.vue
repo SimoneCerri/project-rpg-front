e2e3337510861acca254e8a9a6ded07ec079c861
SimoneCerri <simone.cerri1994@gmail.com>
2024-06-05 16:48:47 +0200

    done

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -1,9 +1,32 @@
 <script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
 
+export default {
+    name: 'AppAbout',
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        const onSwiper = (swiper) => {
+            console.log(swiper);
+        };
+        const onSlideChange = () => {
+            console.log('slide change');
+        };
+        return {
+            onSwiper,
+            onSlideChange,
+        };
+    },
+};
 </script>
-
 <template>
-
+    <swiper :slides-per-view="1" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
+        <swiper-slide><img width="500px" src="/public/img/about.webp" alt=""></swiper-slide>
+        <swiper-slide><img src="/public/img/about.webp" alt=""></swiper-slide>
+        <swiper-slide><img src="/public/background.jpg" alt=""></swiper-slide>
+        ...
+    </swiper>
 </template>
-
-<style></style>
